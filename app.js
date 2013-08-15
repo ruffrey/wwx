7560c5fd517d37f15d6341586c192c123ec0dc53
Jeff Parrish <jeffhparrish@gmail.com>
2013-08-14 19:17:38 -0700

    more node plugging

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.configure(function(){
 	app.use(express.static(__dirname + '/public'));
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
-	app.use(express.bodyParser());
+	//app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.cookieParser());
-	app.use(express.session({ secret: 'asdfjkl' }));
+	//app.use(express.session({ secret: 'asdfjkl' }));
 	app.use(app.router);
 });
 
@@ -29,16 +29,11 @@ app.configure('development', function(){
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
-	app.use(express.errorHandler());
-});
-
 // Routes
 
 app.get('/', routes.index);
 app.get('/contact', routes.contact);
-app.get('/workstuff', routes.workstuff);
-app.get('/yaycode', routes.yaycode);
+app.get('/sacramento-nodejs-development', routes.workstuff);
 
 
 app.listen(process.env.VCAP_APP_PORT || 3000, function(){
